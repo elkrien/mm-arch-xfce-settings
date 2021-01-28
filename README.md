dd03501027fcbf74d7e0a6e67f343888ae07a6db
elkrien <elkrien@gmail.com>
2021-01-28 18:46:05 +0100

    Wersja 1.0.2

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ Wersja 1.0 - 27/01/2021
 Wersja 1.0.1 - 27/01/2021
 - stworzenie skryptu 001 - instalacja środowiska graficznego XFCE z greeterem LightDM
 - stworzenie skryptu 002 - instalacja dźwięku
+
+Wersja 1.0.2 - 27/01/2021
+- dodano skrypty git (git-setup do ustawiania credentials i git-update do automatycznej aktualizacji git)
+
---
+++ git-update.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#set -e
+#################################################################################
+#										#
+#										#
+# Opis		:	Skrypt do update'u git					#
+#		:								#
+# Author	:	Maciej Młocek (thx to Eric Dubois from Arcolinux)	#
+#										#
+#										#
+#################################################################################
+
+
+
+# checking if I have the latest files from github
+echo "Sprawdzanie czy pliki różnią się od github"
+git pull
+
+# Below command will backup everything inside the project folder
+git add --all .
+
+# Give a comment to the commit if you want
+echo "####################################"
+echo "Wprowadź komentarz commit:"
+echo "####################################"
+
+read input
+
+# Committing to the local repository with a message containing the time details and commit text
+
+git commit -m "$input"
+
+# Push the local files to github
+
+git push -u origin master
+
+
+echo "################################################################"
+echo "################    Zaktualizowano git   #######################"
+echo "################################################################"

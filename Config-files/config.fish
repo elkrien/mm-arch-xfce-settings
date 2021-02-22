fafa2c4caae66d6bbd169ce3038164e680b6664b
elkrien <78103450+elkrien@users.noreply.github.com>
2021-02-22 20:17:09 +0100

    1.0.14

--- Config-files/config.fish
+++ Config-files/config.fish
@@ -1,9 +1,25 @@
-# No greeting when starting an interactive shell.
+#### NO GREETING WHEN START
+
 function fish_greeting
 end
 
+#### FISH COLORS
+
 export MICRO_TRUECOLOR=1
 
-# Fish autostart
+
+#### "BAT" AS A MANPAGER
+
+set -x MANPAGER "sh -c 'col -bx | bat -l man -p'" 
+
+
+#### ALIAS
+
+alias cat="bat"
+alias ls="exa -abghHlS --group-directories-first"
+
+#### FISH AUTOSTART
 neofetch
 starship init fish | source
+
+
